cb1977d7eff284f5388f7bb35d1f40dc9f760db9
varadih <helga.varadi1@gmail.com>
2016-12-05 09:36:32 +0100

    second excercise done

--- second.py
+++ second.py
@@ -4,3 +4,14 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_file(file_name, word):
+    try:
+        fw = open(file_name, "w")
+        text = fw.write(word * 10)
+        fw.close()
+        return True
+    except:
+        return False
+
+print(write_file("santa.txt", "hoohoohoo"))
