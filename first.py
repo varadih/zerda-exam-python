56b6c21526c093b33a51253cd9dc77b6ae96f608
varadih <helga.varadi1@gmail.com>
2016-12-05 09:25:49 +0100

    first excercise done

--- first.py
+++ first.py
@@ -2,3 +2,17 @@
 # and returns a new list with every second element from the original list.
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
+
+testlist = [1, 2, 3, 4, 5]
+
+def every_second(input_list):
+    if type(input_list) is list:
+        newlist = []
+        for i in range(len(input_list)):
+            if i % 2 == 1:
+                newlist.append(input_list[i])
+        return newlist
+    else:
+        return ('Please provide a list!')
+
+print(every_second(testlist))
